91ce0b5f967d019a5db97ca10cd30eb67cc378e8
daiki130 <daikibaseball130@gmail.com>
2021-05-16 20:11:33 +0900

    💄 Joinコンポーネントのスタイルを記載

--- src/components/Main/Join/Join.jsx
+++ src/components/Main/Join/Join.jsx
@@ -3,7 +3,31 @@ import "./Join.css";
 export const Join = () => {
   return (
     <>
-      <h1>Joinのページです</h1>
+      <section className="join-main-section">
+        <h1 className="join-text">
+          Join the
+          <span className="accent-text">fun.</span>
+        </h1>
+        <form className="join-form">
+          <div className="input-group">
+            <label>Name:</label>
+            <input type="text" />
+          </div>
+          <div className="input-group">
+            <label>Emaile:</label>
+            <input type="email" />
+          </div>
+          <div className="input-group">
+            <label>Password:</label>
+            <input type="password" />
+          </div>
+          <div className="input-group">
+            <button type="submit" className="btn">
+              Join Now
+            </button>
+          </div>
+        </form>
+      </section>
     </>
   );
 };
