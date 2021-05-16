385b225f0dbf6eb1dcc00e41333f3063613a0b9c
daiki130 <daikibaseball130@gmail.com>
2021-05-16 19:34:11 +0900

    ✨ Footerコンポーネントを作成 Footer.css作成

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,34 +1,40 @@
 import "./Footer.css";
 
-export const Footer = () => {
+export const Footer = (props) => {
+  const { twitter, facebook } = props;
+  const FooterPs = ["About Us", "Contact"];
+  const SocialAppPs = ["Twitter", "Facebook"];
+
   return (
     <>
       <footer className="main-footer">
         <div className="container">
           <nav className="footer-nav">
             <ul>
-              <li>
-                <a href="#">About Us</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
+              {FooterPs.map((p, index) => {
+                return (
+                  <a href="#" key={index}>
+                    {p}
+                  </a>
+                );
+              })}
             </ul>
           </nav>
           <nav className="footer-nav">
             <ul>
-              <li>
-                <a href="#" className="social-link">
-                  <img src="./images/twitter.svg" alt="" />
-                  Twitter
-                </a>
-              </li>
-              <li>
-                <a href="#" className="social-link">
-                  <img src="./images/facebook.svg" alt="" />
-                  Facebook
-                </a>
-              </li>
+              {SocialAppPs.map((App, index) => {
+                return (
+                  <li key={index}>
+                    <a href="#" className="social-link">
+                      <img
+                        src={App === "Twitter" ? twitter : facebook}
+                        alt=""
+                      />
+                      {App}
+                    </a>
+                  </li>
+                );
+              })}
             </ul>
           </nav>
         </div>
